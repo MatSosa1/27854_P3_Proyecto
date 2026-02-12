048589feb85fec8b529897614c868d83ff13b43b
mirandaalison <mirandacreamer@gmail.com>
2026-02-11 20:12:36 -0500

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ module.exports = [
         rules: {
             semi: ['error', 'always'],
             quotes: ['error', 'single'],
-            'comma-dangle': ['error', 'always-multiline'],
+            'comma-dangle': 'off',
         },
     },
 ];
