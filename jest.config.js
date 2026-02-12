825c9964603db991ab160b8d0a670f891f7880b6
mirandaalison <mirandacreamer@gmail.com>
2026-02-11 19:33:45 -0500

    lint correciones

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,28 @@
 module.exports = {
   testEnvironment: 'node',
-  transform: {},
-  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        module: 'commonjs',
+      }
+    }],
+    '^.+\\.js$': 'babel-jest',
+  },
+  testMatch: [
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/'],
   collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/**/*.test.js',
+    'src/**/*.{js,ts}',
+    'Pruebas-Unitarias-Backend/src/**/*.{js,ts}',
+    'Pruebas-Unitarias-Frontend/src/**/*.{js,ts}',
+    '!**/*.test.{js,ts}',
+    '!**/*.spec.{js,ts}',
     '!src/server.js',
     '!src/testRunner.js',
+    '!Pruebas-Unitarias-Backend/src/testRunner.js',
   ],
   coverageThreshold: {
     global: {
